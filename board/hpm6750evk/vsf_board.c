eed34548905cf0d870903de4e482eede0859e5d1
vsfos <vsfos@foxmail.com>
2023-04-24 01:36:35 +0800

    [baord][project] prepare to support HPM6750

---
+++ board/hpm6750evk/vsf_board.c
@@ -0,0 +1,81 @@
+/*****************************************************************************
+ *   Copyright(C)2009-2022 by VSF Team                                       *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *     http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ ****************************************************************************/
+
+/*============================ INCLUDES ======================================*/
+
+#define __VSF_HEAP_CLASS_INHERIT__
+#include "./vsf_board.h"
+
+/*============================ MACROS ========================================*/
+/*============================ MACROFIED FUNCTIONS ===========================*/
+/*============================ TYPES =========================================*/
+/*============================ PROTOTYPES ====================================*/
+/*============================ LOCAL VARIABLES ===============================*/
+/*============================ GLOBAL VARIABLES ==============================*/
+/*============================ LOCAL VARIABLES ===============================*/
+/*============================ IMPLEMENTATION ================================*/
+
+/******************************************************************************/
+/*  DEBUG_STREAM
+/******************************************************************************/
+
+#include "hpm_uart_drv.h"
+#define VSF_DEBUG_STREAM_CFG_RX_BUF_SIZE         32
+static uint8_t __vsf_debug_stream_rx_buff[VSF_DEBUG_STREAM_CFG_RX_BUF_SIZE];
+vsf_mem_stream_t VSF_DEBUG_STREAM_RX = {
+    .op         = &vsf_mem_stream_op,
+    .buffer     = __vsf_debug_stream_rx_buff,
+    .size       = sizeof(__vsf_debug_stream_rx_buff),
+};
+static void __uart_isr(void)
+{
+    uint8_t buff[UART_SOC_FIFO_SIZE], buff_index = 0;
+
+    while (uart_get_irq_id(BOARD_APP_UART_BASE) & uart_intr_id_rx_data_avail) {
+        if (status_success != uart_receive_byte(BOARD_APP_UART_BASE, &buff[buff_index++])) {
+            VSF_ASSERT(false);
+        }
+    }
+    if (buff_index) {
+        VSF_STREAM_WRITE(&VSF_DEBUG_STREAM_RX, buff, buff_index);
+    }
+}
+SDK_DECLARE_EXT_ISR_M(BOARD_APP_UART_IRQ, __uart_isr)
+static void __VSF_DEBUG_STREAM_TX_INIT(void)
+{
+    vsf_stream_connect_tx(&VSF_DEBUG_STREAM_RX.use_as__vsf_stream_t);
+    uart_enable_irq(BOARD_APP_UART_BASE, uart_intr_rx_data_avail_or_timeout);
+    intc_m_enable_irq_with_priority(BOARD_APP_UART_IRQ, 1);
+}
+static void __VSF_DEBUG_STREAM_TX_WRITE_BLOCKED(uint8_t *buf, uint_fast32_t size)
+{
+    for (uint_fast32_t i = 0; i < size; i++) {
+        uart_send_byte(BOARD_APP_UART_BASE, *buf++);
+    }
+}
+
+#include "hal/driver/common/debug_stream/debug_stream_tx_blocked.inc"
+
+
+void vsf_board_init(void)
+{
+    board_init();
+
+    // do not change order below
+    VSF_STREAM_INIT(&VSF_DEBUG_STREAM_RX);
+    VSF_STREAM_INIT(&VSF_DEBUG_STREAM_TX);
+}
---
+++ board/hpm6750evk/vsf_board_cfg.h
@@ -0,0 +1,110 @@
+/*****************************************************************************
+ *   Copyright(C)2009-2022 by VSF Team                                       *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *     http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ ****************************************************************************/
+
+
+//! \note User Level Board Configuration
+
+#ifndef __VSF_BOARD_CFG_H__
+#define __VSF_BOARD_CFG_H__
+
+/*============================ INCLUDES ======================================*/
+
+// retrive compiler information
+#define __VSF_HEADER_ONLY_SHOW_COMPILER_INFO__
+#include "utilities/compiler/compiler.h"
+
+/*============================ MACROS ========================================*/
+
+/*----------------------------------------------------------------------------*
+ * Architecture Configurations                                                *
+ *----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*
+ * Hal Driver Configurations                                                  *
+ *----------------------------------------------------------------------------*/
+
+// Use unused interrupt as SWI
+#define VSF_DEV_SWI_NUM                                 2
+#define VSF_DEV_SWI0_IRQ                                IRQn_UART14
+#define VSF_DEV_SWI1_IRQ                                IRQn_UART15
+
+// actually general hal components will be enabled by default
+#ifndef VSF_HAL_USE_RNG
+#   define VSF_HAL_USE_RNG                              ENABLED
+#endif
+#define VSF_HAL_USE_GPIO                                ENABLED
+#   define VSF_HW_GPIO_CFG_MULTI_CLASS                  ENABLED
+#ifndef VSF_HAL_USE_SPI
+#   define VSF_HAL_USE_SPI                              ENABLED
+#endif
+#ifndef VSF_HAL_USE_MMC
+#   define VSF_HAL_USE_MMC                              ENABLED
+#endif
+#ifndef VSF_HAL_USE_I2S
+#   define VSF_HAL_USE_I2S                              ENABLED
+#endif
+#ifndef VSF_HAL_USE_USART
+#   define VSF_HAL_USE_USART                            ENABLED
+#endif
+#ifndef VSF_HAL_USE_DEBUG_STREAM
+#   define VSF_HAL_USE_DEBUG_STREAM                     ENABLED
+#endif
+
+/*----------------------------------------------------------------------------*
+ * Kernel Configurations                                                      *
+ *----------------------------------------------------------------------------*/
+
+#if __IS_COMPILER_GCC__ || __IS_COMPILER_LLVM__
+// strtoxxx in newlib has dependency issues, use implementation in simple_stdlib.
+//  useful only if VSF_USE_LINUX/VSF_LINUX_USE_SIMPLE_LIBC/VSF_LINUX_USE_SIMPLE_STDLIB are enabled
+#   define VSF_LINUX_SIMPLE_STDLIB_USE_STRTOXX          ENABLED
+#endif
+
+// configure pool and heap to avoid heap allocating in interrupt
+#define VSF_OS_CFG_EVTQ_POOL_SIZE                       128
+#define VSF_OS_CFG_DEFAULT_TASK_FRAME_POOL_SIZE         32
+#define VSF_POOL_CFG_FEED_ON_HEAP                       DISABLED
+
+/*----------------------------------------------------------------------------*
+ * Components Configurations                                                  *
+ *----------------------------------------------------------------------------*/
+
+#define VSF_USE_HEAP                                    ENABLED
+#define VSF_HEAP_CFG_MCB_ALIGN_BIT                      4
+#   define VSF_HEAP_SIZE                                (128 * 1024)
+
+// debug stream is implemented in vsf_board.c
+#if VSF_HAL_USE_DEBUG_STREAM == DISABLED
+#   define VSF_CFG_DEBUG_STREAM_TX_T                    vsf_stream_t
+#   define VSF_CFG_DEBUG_STREAM_RX_T                    vsf_mem_stream_t
+#endif
+
+#define VSF_LINUX_CFG_STACKSIZE                         (8 * 1024)
+#define VSF_LINUX_USE_SIMPLE_LIBC                       ENABLED
+#define VSF_USE_SIMPLE_SPRINTF                          ENABLED
+#define VSF_USE_SIMPLE_SSCANF                           ENABLED
+
+/*----------------------------------------------------------------------------*
+ * Application Configurations                                                 *
+ *----------------------------------------------------------------------------*/
+
+/*============================ TYPES =========================================*/
+/*============================ GLOBAL VARIABLES ==============================*/
+/*============================ LOCAL VARIABLES ===============================*/
+/*============================ PROTOTYPES ====================================*/
+
+#endif      // __VSF_BOARD_CFG_H__
